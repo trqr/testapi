453ca5a0fd8d601d1b2163fcaa0a6fc60d6c1c3c
TrqR <tristanqr@gmail.com>
2025-05-16 15:38:34 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,88 @@
+
+
+let fetchedData = [];
+let allMoviesData = [];
+let compteurPage = 1;
+
+function renderCards(){
+
+let html = ""
+
+allMoviesData.forEach(element => {
+    html += 
+    `
+    <div class="card cell">
+        <div class="card-image">
+            <figure class="image">
+            <img
+                src="https://image.tmdb.org/t/p/w500${element.poster_path}"
+                alt="${element.original_title}"
+            />
+            </figure>
+        </div>
+        <div class="card-content">
+            <div class="media">
+            <div class="media-left">
+                <figure class="image is-48x48">
+                <img
+                    src="https://image.tmdb.org/t/p/w500${element.backdrop_path}"
+                    alt="Placeholder image"
+                />
+                </figure>
+            </div>
+            <div class="media-content">
+                <p class="title is-4">${element.original_title}</p>
+                <p class="subtitle is-6">${element.release_date}</p>
+            </div>
+            </div>
+
+            <div class="content">
+            ${element.overview}
+            <br />
+            <time datetime="${element.release_date}">${element.release_date}</time>
+            </div>
+        </div>
+    </div>
+    `
+});
+    document.querySelector('.cards-container').innerHTML = html;
+
+};
+
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNjJhMzBiMzIwMDc4OTc3YzQ5ODg1MTk3ZmIwYzE0ZSIsIm5iZiI6MTc0NDQ2NDcwMi40NjUsInN1YiI6IjY3ZmE2YjNlZDNhYjdkN2E4YmFkZGFlYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Xb9GpGoRsKOQ2pIdndHarxCMUHzlD6oc2uxjz9QxHg0'
+  }
+};
+
+function fetchingAndRendering(url){
+    fetch(url, options)
+        .then(res => res.json())
+        .then(res => {
+            fetchedData = res.results;
+            fetchedData.forEach(movie => allMoviesData.push(movie))
+            console.log('Data stored in variable:', allMoviesData);
+            renderCards()
+        })
+        .catch(err => console.error(err));
+    }
+
+fetchingAndRendering('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1')
+
+window.addEventListener('scroll', function() {
+    if (window.innerHeight + window.scrollY >= document.documentElement.scrollHeight - 30) {
+        if (!document.getElementById('bottom-message')) {
+
+            compteurPage++;
+            fetchingAndRendering(`https://api.themoviedb.org/3/movie/popular?language=en-US&page=${compteurPage}`)
+            const loadingToast = document.querySelector('.loading-toast')
+            loadingToast.classList.remove('is-hidden');
+            setTimeout(() => {
+                loadingToast.classList.add('is-hidden');
+            }, 2000);
+        }
+    }
+});
+
